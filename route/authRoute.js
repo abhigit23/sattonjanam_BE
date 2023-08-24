f326c1feac9c89f2ae7584664759210867952e8d
Divatta <119591029+Divatta@users.noreply.github.com>
2023-08-24 17:30:11 +0530

    Update authRoute.js

--- route/authRoute.js
+++ route/authRoute.js
@@ -5,6 +5,10 @@ router.post(`/register`, authController.register)
 
 router.post(`/login`, authController.login)
 
+router.get(`/auth/authToken`, authController.authToken)
+
+router.get(`/auth/currentUser`, auth , authController.currentUser)
+
 router.get(`/user/all`, authController.getAll)
 
 router.get(`/user/single/:id`, authController.getSingle)
