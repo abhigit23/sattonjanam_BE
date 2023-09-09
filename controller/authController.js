2b5ab7bc4489733d304e6b12dae1f66197344bd7
Divatta <divattadil@gmail.com>
2023-09-09 12:05:55 +0530

    updated connectus mail

--- controller/authController.js
+++ controller/authController.js
@@ -183,7 +183,7 @@ const authController = {
           <div style="margin: 20px;">
             <h3>Dear ${req.body.userName},</h3>
             <p>Thank you for contacting with sattonjanam.com</p>
-            <p>You are very improtant to us, all information received will always remain confidential.</p>
+            <p>You are very important to us, all information received will always remain confidential.</p>
             <p>We will contact you as soon as we review your message or you can reach us on 9773643677</p>
           </div>
       `,
